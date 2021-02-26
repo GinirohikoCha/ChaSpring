d2db5ffad606e71d3bb5b998fbad74a34a6e0d8c
GinirohikoCha <rinkouatf@qq.com>
2021-02-26 23:22:48 +0800

    添加MybatisPlusConfig & 重构xml文件位置

---
+++ src/main/java/com/github/ginirohikocha/spring/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.github.ginirohikocha.spring.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author GinirohikoCha
+ * @version 0.0.1
+ * @date 2021/2/26 9:07
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+}
